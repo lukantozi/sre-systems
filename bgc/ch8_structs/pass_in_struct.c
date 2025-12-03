3c51c1af1f67e3a5b7ede6ff47ed7d7f0ad0829c
lukantozi <lukamamri@outlook.com>
2025-12-03 16:59:32 +0100

    start chapter 9

---
+++ bgc/ch8_structs/pass_in_struct.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct car {
+    char *name;
+    float price;
+    int speed;
+};
+
+void set_price(struct car *c, float new_price) {
+    c->price = new_price;
+}
+
+int main(void) {
+    struct car saturn = {.speed=175, .name="Saturn SL/2"};
+
+    set_price(&saturn, 799.99);
+    printf("Price: %f\n", saturn.price);
+}
