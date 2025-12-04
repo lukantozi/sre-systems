bbb6e989c77859eac281d1e9b3dd5d3b40ee3f20
lukantozi <lukamamri@outlook.com>
2025-12-04 20:35:23 +0100

    finish chapter 9

--- bgc/ch9_file_IO/binary.c
+++ bgc/ch9_file_IO/binary.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     FILE *fp;
-    unsigned char bytes[6] = {5, 37, 0, 88, 255, 32};
+    unsigned char bytes[6] = {5, 37, 0, 88, 255, 12};
 
     fp = fopen("output.bin", "wb");
 
