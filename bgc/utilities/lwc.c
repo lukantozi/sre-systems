ec171a296d0592030150b0666644ac6e7be77ae0
lukantozi <lukamamri@outlook.com>
2025-12-09 21:27:24 +0100

    lwc (wc replication) can print words, lines, characters

---
+++ bgc/utilities/lwc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Please specify the file to operate on.\n");
+        return 0;
+    }
+
+    int c;
+    int line_count = 0;
+    int char_count = 0;
+    int word_count = 1;
+    int space_flag = 0;
+
+    FILE *pf = fopen(argv[1], "r");
+    while ((c = fgetc(pf)) != EOF) {
+        char_count++;
+        if (!((c >= 0x09 && c <= 0x0D) || c == 0x20)) {
+            if (space_flag == 1) {
+                word_count++;
+                space_flag = 0;
+            }
+        }
+        if (c == '\n') {
+            line_count++;
+        }
+        if ((c >= 0x09 && c <= 0x0D) || c == 0x20) {
+                space_flag = 1;
+        }
+    }
+    printf("total lines = %d\n", line_count);
+    printf("total words = %d\n", word_count);
+    printf("total characters = %d\n", char_count);
+}
