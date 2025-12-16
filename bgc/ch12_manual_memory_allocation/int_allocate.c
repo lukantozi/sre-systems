3fb7d67f83661c808e2cff0ebb6d70262f492647
lukantozi <lukamamri@outlook.com>
2025-12-16 20:23:16 +0100

    finish manual memory alloc

--- bgc/ch12_manual_memory_allocation/int_allocate.c
+++ bgc/ch12_manual_memory_allocation/int_allocate.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-    int *p = malloc(sizeof(int));
+    int *p = malloc(sizeof(int)); //sizeof *p -> will work too because, constant expression *p evaluates to int type
     *p = 12;
     printf("%d\n", *p);
     free(p);
