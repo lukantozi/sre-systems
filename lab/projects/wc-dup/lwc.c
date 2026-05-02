300ec5f4f58d2d3b72ef5145afe4572bd2ad602d
lukantozi <lukamamrik@proton.me>
2026-05-02 12:10:20 +0200

    fix wc dupe

--- lab/projects/wc-dup/lwc.c
+++ lab/projects/wc-dup/lwc.c
@@ -9,10 +9,14 @@ int main(int argc, char **argv) {
     int c;
     int line_count = 0;
     int char_count = 0;
-    int word_count = 1;
+    int word_count = 0;
     int space_flag = 0;
 
     FILE *pf = fopen(argv[1], "r");
+    if (pf == NULL) {
+        fprintf(stderr, "Error: cannot open file\n");
+        return 1;
+    }
     while ((c = fgetc(pf)) != EOF) {
         char_count++;
         if (!((c >= 0x09 && c <= 0x0D) || c == 0x20)) {
@@ -28,6 +32,7 @@ int main(int argc, char **argv) {
                 space_flag = 1;
         }
     }
+    fclose(pf);
     printf("total lines = %d\n", line_count);
     printf("total words = %d\n", word_count);
     printf("total characters = %d\n", char_count);
