1aab794bc960929da0f5dccec6944ec4952f8165
lukantozi <lukamamrik@proton.me>
2026-05-09 00:56:24 +0200

    fix loop

--- problems/leetcode/0283_move_zeroes.c
+++ problems/leetcode/0283_move_zeroes.c
@@ -6,7 +6,7 @@ void moveZeroes(int *nums, int numsSize) {
     for (i = numsSize-1; i >= 0; i--) {
         if (nums[i] == 0) {
             j = i;
-            while (nums[j] != 0 || j + 1 < numsSize) {
+            while (j + 1 < numsSize) {
                 tmp = nums[j];
                 nums[j] = nums[j+1];
                 nums[j+1] = tmp;
